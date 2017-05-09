7046076132667f974265690929469cfbb951c5fd
Kelsey MacMillan <kelsey.macmillan@gmail.com>
2017-05-09 15:10:10 -0700

    up to 30 reps

--- pubmed_lda.py
+++ pubmed_lda.py
@@ -82,7 +82,7 @@ if __name__ == "__main__":
     df = pd.DataFrame()
 
     # Iterate
-    for rep in range(3):
+    for rep in range(30):
 
         # Run model
         avg_score, n_resolved = main(docs, labels, all_labels)
