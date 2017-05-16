5f1e5fd269882baa8a9137dc830881d2cd097566
Kelsey MacMillan <kelsey.macmillan@gmail.com>
2017-05-16 12:22:25 -0700

    try pubmed with no error weighting

--- src/model/ws_nmf.py
+++ src/model/ws_nmf.py
@@ -31,12 +31,12 @@ class Model:
         """
         L = np.ones((len(self.labels), self.K)) * 1  # initialize matrix of ones
 
-        for document_index, topic_index_list in enumerate(self.labels):
-            if len(topic_index_list) > 0:  # if document has been labeled
-                L[document_index, :] = 0  # set all labels to zero for that doc initially
-
-                for topic_index in topic_index_list:
-                    L[document_index, topic_index] = 1  # set labeled topic / document to 1
+        # for document_index, topic_index_list in enumerate(self.labels):
+        #     if len(topic_index_list) > 0:  # if document has been labeled
+        #         L[document_index, :] = 0  # set all labels to zero for that doc initially
+        #
+        #         for topic_index in topic_index_list:
+        #             L[document_index, topic_index] = 1  # set labeled topic / document to 1
         return L
 
     def initialize_H(self):
